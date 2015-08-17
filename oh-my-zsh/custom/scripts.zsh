c39466143afc780f60253086a7b01e917810a84a
Ian Lai <MrSaints@users.noreply.github.com>
2015-08-17 21:32:52 +0100

    Added CHMOD fix script

--- oh-my-zsh/custom/scripts.zsh
+++ oh-my-zsh/custom/scripts.zsh
@@ -16,6 +16,13 @@ alias perms="stat --format '%a'"
 # IRC client
 alias irc="weechat"
 
+# Set directories to 755 and files to 644
+# Use with caution! (do not execute in root)
+fixchmod() {
+    sudo find * -type d -print0 | xargs -0 chmod 0755
+    sudo find . -type f -print0 | xargs -0 chmod 0644
+}
+
 # Clean
 quickclean() {
     sudo apt-get autoclean
