a8178ef51679ee8a3b004247d2aea269552abcfe
Ian Lai <MrSaints@users.noreply.github.com>
2015-08-24 04:56:50 +0100

    Set config upon installation

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -11,3 +11,12 @@ nvm alias default stable
 # Ensure the following is in ~/.bash_profile, ~/.zshrc or ~/.profile
 # export NVM_DIR="/home/mrsaints/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Sets npm init defaults if nvm is properly installed
+if type npm >/dev/null 2>&1
+then
+    npm config set init.author.name "Ian Lai"
+    npm config set init.author.email os@fyianlai.com
+    npm config set init.author.url https://www.fyianlai.com/
+    npm config set init.license MIT
+fi
