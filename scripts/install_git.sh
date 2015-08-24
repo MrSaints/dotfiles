9dfa7ba893d5215ba52ca152c86992e968c9aed7
Ian Lai <MrSaints@users.noreply.github.com>
2015-08-24 04:57:03 +0100

    Added script to install git, and set global config

---
+++ scripts/install_git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Installs git
+sudo apt-get install git
+
+if type git >/dev/null 2>&1
+then
+    git config --global user.name "Ian Lai"
+    git config --global user.email MrSaints@users.noreply.github.com
+fi
