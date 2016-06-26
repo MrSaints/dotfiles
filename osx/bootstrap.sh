69c4ae977aad2f07b9abf18f7d6b736d0e897741
Ian Lai <MrSaints@users.noreply.github.com>
2016-06-26 17:05:41 +0100

    Update MacOS dotfiles

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -9,6 +9,15 @@ defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+## Increase speed
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+# Keyboard
+## Increase speed
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
 
 # Finder
 ## Show hidden files
@@ -17,3 +26,33 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 ## Empty the trash securely
 defaults write com.apple.finder EmptyTrashSecurely -bool true
+## Disable animation
+defaults write com.apple.finder DisableAllAnimations -bool true
+## Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+## Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Dock
+## Auto show/hide the Dock
+defaults write com.apple.dock autohide -bool true
+## Remove auto show/hide delay
+defaults write com.apple.dock autohide-delay -float 0
+## Remove auto show/hide animation
+defaults write com.apple.dock autohide-time-modifier -float 0
+## Use scale for minimize/maximize effect
+defaults write com.apple.dock mineffect -string "scale"
+## Shrink size of Dock items / icons
+defaults write com.apple.dock tilesize -int 16
+
+# Screen
+## Prompt for password immediately
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+## Disable dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Photos
+## Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
--- osx/install_brew.sh
+++ osx/install_brew.sh
@@ -6,33 +6,33 @@ brew update
 brew upgrade --all
 
 # Extras
-brew tap homebrew/versions
 brew tap caskroom/cask
+brew tap homebrew/versions
 
 # Utils
 brew install coreutils
-brew install wget
 brew install parallel
 brew install the_platinum_searcher
+brew install wget
 
 # Git
 brew install git
 brew install git-extras
 
 # Languages
-brew install go
-# brew install nvm
 # brew cask install java
+# brew install nvm
+brew install go
 
 # Apps
-brew cask install google-chrome-beta
-brew cask install sublime-text3
-brew cask install iterm2
+# brew cask install virtualbox
+# brew cask install vivaldi
 brew cask install amethyst
 brew cask install flux
-brew cask install virtualbox
+brew cask install google-chrome-beta
 brew cask install hipchat
-# brew cask install vivaldi
+brew cask install iterm2
+brew cask install sublime-text3
 
 # Workflow
 brew cask install heroku-toolbelt
