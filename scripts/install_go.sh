d5645b3765f2e553ff5b443e1807ec85104f60b7
Ian Lai <MrSaints@users.noreply.github.com>
2016-06-26 17:06:44 +0100

    Update Go autoinstall

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,5 +2,9 @@
 
 # Installed using godeb
 # Source: https://github.com/niemeyer/godeb
-# Future: https://github.com/travis-ci/gimme
-wget -O - --no-check-certificate "https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz" | tar -xvzf - && ./godeb install
+# Future (?): https://github.com/travis-ci/gimme
+# wget -O - --no-check-certificate "https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz" | tar -xvzf - && ./godeb install
+
+chmod +x godeb
+
+./godeb install 1.6.2
