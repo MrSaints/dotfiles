f8f6e14b286a2e95a8ba9ed18e8e77beaf8e7fab
Ian Lai <MrSaints@users.noreply.github.com>
2016-08-28 02:33:01 +0100

    Update bootstrap.sh to load scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,3 +2,10 @@
 
 # Create directory for screenshots
 mkdir -p "$HOME/Pictures/Screenshots/"
+
+# Run scripts
+./install_git.sh
+./install_zsh.sh
+./install_misc.sh
+./install_go.sh
+./install_node.sh
