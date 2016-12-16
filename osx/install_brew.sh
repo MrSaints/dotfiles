481853e3155c1b2ca9dbd2155a1dffec6516a4e6
Ian Lai <MrSaints@users.noreply.github.com>
2016-12-16 00:02:53 +0000

    Remove hipchat, livereload from Brew installations, and add jid
    - `jid` is useful for exploring a JSON dump
    - By default, I do not want hipchat or livereload on my MacOS
    - Add sbt, but commented out as it is not needed by default
      (since I do not work with Scala that often)
    - Update Heroku toolbelt to use main Brew rather than Cask

--- osx/install_brew.sh
+++ osx/install_brew.sh
@@ -10,7 +10,8 @@ brew tap caskroom/cask
 brew tap homebrew/versions
 
 # Utils
-brew install coreutils
+brew install coreutils --with-default-names
+brew install jid
 brew install parallel
 brew install the_platinum_searcher
 brew install wget
@@ -22,23 +23,24 @@ brew install git-extras
 # Languages
 # brew cask install java
 # brew install nvm
+# brew install sbt
 brew install go
 
 # Apps
+# brew cask install hipchat
 # brew cask install virtualbox
 # brew cask install vivaldi
 brew cask install amethyst
 brew cask install flux
 brew cask install google-chrome-beta
-brew cask install hipchat
 brew cask install iterm2
 brew cask install sublime-text3
 
 # Workflow
-brew cask install heroku-toolbelt
+brew install heroku-toolbelt
 heroku update
 
-brew cask install livereload
+# brew cask install livereload
 
 # Clean up
 brew cleanup
