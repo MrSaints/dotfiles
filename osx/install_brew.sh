01b11db06a8aba018954c76fbacf2005b0537b1f
Ian Lai <ian@fyianlai.com>
2017-10-26 18:27:34 +0100

    Update OSX scripts

--- osx/install_brew.sh
+++ osx/install_brew.sh
@@ -12,10 +12,13 @@ brew tap homebrew/versions
 
 # Utils
 brew install coreutils --with-default-names
+brew install ffmpeg
 brew install jid
+brew install keybase
 brew install parallel
 brew install the_platinum_searcher
 brew install wget
+# brew install gpg
 
 # Git
 brew install git
@@ -23,20 +26,24 @@ brew install git-extras
 
 # Languages
 # brew cask install java
+# brew install ghostscript
 # brew install nvm
 # brew install sbt
 brew install go
 
 # Apps
-# brew cask install hipchat
+# brew cask install slack
 # brew cask install virtualbox
 # brew cask install vivaldi
-brew cask install amethyst
-brew cask install flux
 brew cask install google-chrome-beta
 brew cask install iterm2
 brew cask install sublime-text3
+brew cask install vlc
+
+brew cask install amethyst
 brew cask install fluor
+brew cask install flux
+brew cask install smcfancontrol
 
 # Workflow
 brew install heroku-toolbelt
