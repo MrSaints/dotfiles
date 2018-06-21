8880bb01aa47fa296efb4060bf3b2ed94e02285d
Ian Lai <ian@fyianlai.com>
2018-06-21 04:30:37 +0100

    Add Google Cloud SDK bin

--- oh-my-zsh/custom/paths.zsh
+++ oh-my-zsh/custom/paths.zsh
@@ -1,6 +1,6 @@
 # Golang
 export GOPATH=$HOME/workspace/golang
-export PATH=$PATH:$GOPATH/bin:~/bin:~/.yarn/bin
+export PATH=$PATH:$GOPATH/bin:~/bin:~/.yarn/bin:/opt/google-cloud-sdk/bin/
 
 # PHP (Composer)
 export PATH=$PATH:~/.composer/vendor/bin
