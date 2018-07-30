9f928f541187ca645440c29fec3064b14cb2de08
Ian Lai <ian@fyianlai.com>
2018-07-30 10:35:53 +0100

    Move startup / init config to i3 config
    Also, set terminal for i3 to termite.

--- arch/arandr/arachnys.sh
+++ arch/arandr/arachnys.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 xrandr --output HDMI-2 --off --output HDMI-1 --off
+
 xrandr --output eDP-1 --primary --mode 1280x800 --pos 1920x1440 --rotate normal
+
 xrandr --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal
 xrandr --output DP-2  --mode 2560x1440 --pos 2560x0 --rotate normal
