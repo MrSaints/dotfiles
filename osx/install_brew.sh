37b9786c69f9e9c0594f903a967c9a0a64014579
Ian Lai <ian@fyianlai.com>
2018-12-11 21:18:59 +0000

    Update brew installation script
    Remove old, unused packages, and add new ones.

--- osx/install_brew.sh
+++ osx/install_brew.sh
@@ -3,26 +3,28 @@
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew upgrade --all
-brew cask update
 
 # Extras
 brew tap caskroom/cask
-brew tap homebrew/versions
+brew tap homebrew/cask-versions
 
 # Utils
 brew install coreutils --with-default-names
-brew install ffmpeg
 brew install jid
-brew install keybase
 brew install parallel
-brew install the_platinum_searcher
 brew install wget
-# brew install gpg
+brew install ripgrep
+brew install diff-so-fancy
+# brew install ffmpeg
+# brew install the_platinum_searcher
+
+brew install keybase
+brew install gpg
+brew install pinentry-mac
 
 # Git
 brew install git
-brew install git-extras
+# brew install git-extras
 
 # Languages
 # brew cask install java
@@ -37,19 +39,23 @@ brew install go
 # brew cask install vivaldi
 brew cask install google-chrome-beta
 brew cask install iterm2
-brew cask install sublime-text3
+brew cask install sublime-text
 brew cask install vlc
 
 brew cask install amethyst
-brew cask install fluor
-brew cask install flux
-brew cask install smcfancontrol
+# brew cask install fluor
+# brew cask install flux
+# brew cask install smcfancontrol
 
 # Workflow
 # brew install heroku-toolbelt
 # heroku update
 
 # brew cask install livereload
+
+# Upgrade
+brew upgrade --all
+brew cask upgrade
 
 # Clean up
 brew cleanup
