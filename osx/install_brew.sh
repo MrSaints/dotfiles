53190f77f3f172a9171ee8603f530a3447576110
Ian Lai <ian@fyianlai.com>
2020-03-08 21:19:53 +0000

    Update brew packages

--- osx/install_brew.sh
+++ osx/install_brew.sh
@@ -12,7 +12,7 @@ brew tap homebrew/cask-versions
 brew install coreutils --with-default-names
 brew install parallel
 brew install wget
-# brew install ffmpeg
+brew install ffmpeg
 
 brew install bat
 brew install diff-so-fancy
@@ -20,50 +20,41 @@ brew install jid
 brew install jq
 brew install ripgrep
 brew install xsv
-# brew install the_platinum_searcher
 
+brew install kryptco/tap/kr
 brew install gpg
 brew install pinentry-mac
 brew install mkcert
 
 # Kubernetes
+brew install kubernetes-cli
 brew install kubernetes-helm
 brew install ksonnet/tap/ks
 brew install derailed/k9s/k9s
+brew install kubectx
 brew install octant
 
 # Git
 brew install git
-# brew install git-extras
 
 # Languages
-# brew cask install java
-# brew install ghostscript
-# brew install nvm
-# brew install sbt
 brew install go
+brew cask install java
 
 # Apps
-# brew cask install virtualbx
-# brew cask install vivaldi
-brew cask install google-chrome-beta
 brew cask install iterm2
+brew cask install sublime-text
+brew cask install dbeaver-community
+brew cask install google-chrome-beta
 brew cask install keybase
 brew cask install slack
 brew cask install spotify
-brew cask install sublime-text
 brew cask install vlc
 
 brew cask install amethyst
+
+# No touch bar support
 # brew cask install fluor
-# brew cask install flux
-# brew cask install smcfancontrol
-
-# Workflow
-# brew install heroku-toolbelt
-# heroku update
-
-# brew cask install livereload
 
 # Upgrade
 brew upgrade
