c18d534c2a284699c0bfcddc66a829fb3ead4c37
Ian Lai <ian@fyianlai.com>
2020-06-16 18:05:11 +0100

    Install Thunar, Chrome, and ST3

--- install/linux/08_install_apps.sh
+++ install/linux/08_install_apps.sh
@@ -9,5 +9,24 @@ if [[ $REV = *"ARCH"* ]]; then
 elif hash apt 2>/dev/null; then
     echo "Debian"
 
-    # TODO
+    # TODO: termite
+    # sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
+
+    # krypt.co
+    # curl https://krypt.co/kr | sh
+
+    # Thunar File Manager
+    sudo apt install -y thunar
+    xdg-mime default Thunar.desktop inode/directory
+
+    # Google Chrome Beta
+    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+    # Sublime Text
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+    sudo apt update -y
+    sudo apt install -y google-chrome-beta sublime-text
 fi
