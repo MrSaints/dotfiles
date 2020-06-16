6c0f55318f0b28e0a704532b1f3a6e9d40ba7cf4
Ian Lai <ian@fyianlai.com>
2020-06-16 13:51:22 +0100

    Bump Python version

--- install/programming/01_install_python.sh
+++ install/programming/01_install_python.sh
@@ -7,7 +7,7 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 # Ensure `pyenv init` is in ~/.bash_profile, ~/.zshrc or ~/.profile
 # e.g. echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
-pyenv install 3.7.5
-pyenv global 3.7.5
+pyenv install 3.7.7
+pyenv global 3.7.7
 
 pip install --upgrade pip
