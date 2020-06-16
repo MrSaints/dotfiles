195874e4ce07667504abb3f45d59e4c75626860c
Ian Lai <ian@fyianlai.com>
2020-06-16 23:12:09 +0100

    Install `fd`

--- install/programming/03_install_rust.sh
+++ install/programming/03_install_rust.sh
@@ -6,4 +6,5 @@ curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
 hash bat 2>/dev/null || cargo install --locked bat
 hash delta 2>/dev/null || cargo install --locked git-delta
 hash exa 2>/dev/null || cargo install --locked exa
+hash fd 2>/dev/null || cargo install --locked fd-find
 hash ripgrep 2>/dev/null || cargo install --locked ripgrep
