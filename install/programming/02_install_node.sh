50645edcd5840e19c2a97be1ab764f0eadddd99a
Ian Lai <ian@fyianlai.com>
2020-08-07 17:28:02 +0100

    Add note that NVM is already loaded through zsh custom

--- install/programming/02_install_node.sh
+++ install/programming/02_install_node.sh
@@ -8,6 +8,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install stable
 nvm alias default stable
 
+# NOTE: the following is already in `custom/100_init.zsh`
 # Ensure the following is in ~/.bash_profile, ~/.zshrc or ~/.profile
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
