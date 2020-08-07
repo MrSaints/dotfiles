1f08b77796fcbbe5a97625c95e87327c2b8ae9c0
Ian Lai <ian@fyianlai.com>
2020-08-07 17:27:36 +0100

    Ensure `pyenv` is loaded before upgrading `pip`

--- install/programming/01_install_python.sh
+++ install/programming/01_install_python.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+f#!/bin/bash
 
 # Installed using pyenv
 # Source: https://github.com/pyenv/pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+# NOTE: the following is already in `custom/100_init.zsh`
 # Ensure `pyenv init` is in ~/.bash_profile, ~/.zshrc or ~/.profile
 # e.g. echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
 pyenv install 3.7.7
 pyenv global 3.7.7
+
+eval "$(pyenv init -)"
 
 pip install --upgrade pip
