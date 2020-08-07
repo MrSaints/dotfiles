689fce70ce28e11e9ceaf55f8906570a9a53457f
Ian Lai <ian@fyianlai.com>
2020-08-07 17:28:42 +0100

    Check if Go is installed

--- install/programming/04_install_go.sh
+++ install/programming/04_install_go.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "Manual install: https://golang.org/doc/install"
+if ! type "go" > /dev/null; then
+    echo "Go is not installed. Manually install: https://golang.org/doc/install"
+    exit 1
+fi
 
 go get -d -v golang.org/x/tools/cmd/guru
