b7e53b14d2cda1534dee555c9e250bc6097f5fef
Ian Lai <ian@fyianlai.com>
2021-05-31 22:56:14 +0100

    Set-up dynamic titles with Alacritty + zsh

--- zsh/100_init.zsh
+++ zsh/100_init.zsh
@@ -7,3 +7,23 @@ eval "$(starship init zsh)"
 
 # History
 eval "$(atuin init zsh)"
+
+# Dynamic title
+# https://wiki.gentoo.org/wiki/Alacritty#Zsh
+if [[ "${TERM}" != "" && "${TERM}" == "alacritty" ]]
+then
+    precmd()
+    {
+        # output on which level (%L) this shell is running on.
+        # append the current directory (%~), substitute home directories with a tilde.
+        # "\a" bell (man 1 echo)
+        # "print" must be used here; echo cannot handle prompt expansions (%L)
+        print -Pn "\e]0;$(id --user --name)@$(hostname): zsh[%L] %~\a"
+    }
+
+    preexec()
+    {
+        # output current executed command with parameters
+        echo -en "\e]0;$(id --user --name)@$(hostname): ${1}\a"
+    }
+fi
